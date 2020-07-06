d9f7ee0c12653d7f1f7b8468e147640f9fef7d38
CHJohnston <cdh.johnston@gmail.com>
2020-07-06 13:26:44 -0500

    Add project files.

---
+++ StaticExerciseConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StaticExerciseConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Temperature Conversion");
+            //Convert Fahrenheit to Celsius
+            Console.Write("Enter a Temperature in Fahrenheit to Convert to Celsius: ");                
+            double ftemp = TempConverter.FahrenheitToCelsius(double.Parse(Console.ReadLine()));
+            Console.WriteLine($"The Celsius Temperature is {ftemp} degrees.");
+            Console.WriteLine();
+
+            //Convert Celsius to Fahrenheit
+            Console.Write("Enter a Temperature in Celsius to convert to Fahrenheit: ");
+            double ctemp = TempConverter.CelsiusToFahrenheit(double.Parse(Console.ReadLine()));
+            Console.WriteLine($"The Fahrenheit Temperature is {ctemp} degrees.");
+            
+        }
+    }
+}
---
+++ StaticExerciseConsoleUI/TempConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExerciseConsoleUI
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit) 
+        {            
+            return (fahrenheit - 32) * 5/9;
+        }
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9/5) +32;
+        }
+    }
+}
